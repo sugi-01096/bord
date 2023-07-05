b171284fbe18ca2e8ba26e108cbe2c57ae476a8d
sugi-01096 <nodoguro04@gmail.com>
2023-07-05 17:17:52 +0900

    ajb

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import streamlit as st
 import json
 
 
-def save_post(title, content):
-    post = {"title": title, "content": content}
+def save_post(content):
+    post = { "content": content}
     with open('posts.json', 'a') as file:
         json.dump(post, file)
         file.write('\n')
@@ -15,15 +15,13 @@ def load_posts():
 
 
 def main():
-    st.title("掲示板アプリ")
 
     # 新規投稿の入力
-    new_post_title = st.text_input("タイトル")
     new_post_content = st.text_area("新規投稿", height=100)
 
     # 投稿ボタンが押された場合
-    if st.button("投稿する") and new_post_title and new_post_content:
-        save_post(new_post_title, new_post_content)
+    if st.button("投稿する") and new_post_content:
+        save_post( new_post_content)
         st.success("投稿が保存されました！")
 
     # 保存された投稿の表示
@@ -34,7 +32,6 @@ def main():
         st.info("まだ投稿がありません。")
     else:
         for post in posts:
-            st.text(post["title"])
             st.text(post["content"])
             st.markdown("---")
 
