7e79b8ad7b6e0a7386aa3941ac59425c1329685d
sugi-01096 <nodoguro04@gmail.com>
2023-07-05 17:16:31 +0900

    kfn

--- main.py
+++ main.py
@@ -1,8 +1,43 @@
 import streamlit as st
+import json
+
+
+def save_post(title, content):
+    post = {"title": title, "content": content}
+    with open('posts.json', 'a') as file:
+        json.dump(post, file)
+        file.write('\n')
+
+
+def load_posts():
+    with open('posts.json', 'r') as file:
+        return [json.loads(line) for line in file]
+
 
 def main():
-    st.title('Hello, World!')
-    st.write('This is a simple example of a Streamlit app.')
+    st.title("掲示板アプリ")
 
-if __name__ == '__main__':
+    # 新規投稿の入力
+    new_post_title = st.text_input("タイトル")
+    new_post_content = st.text_area("新規投稿", height=100)
+
+    # 投稿ボタンが押された場合
+    if st.button("投稿する") and new_post_title and new_post_content:
+        save_post(new_post_title, new_post_content)
+        st.success("投稿が保存されました！")
+
+    # 保存された投稿の表示
+    posts = load_posts()
+    st.subheader("保存された投稿")
+
+    if not posts:
+        st.info("まだ投稿がありません。")
+    else:
+        for post in posts:
+            st.text(post["title"])
+            st.text(post["content"])
+            st.markdown("---")
+
+
+if __name__ == "__main__":
     main()
