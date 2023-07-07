643c4871c408dce551aafd22deff643f018630d5
sugi-01096 <132065223+sugi-01096@users.noreply.github.com>
2023-07-07 13:55:38 +0900

    ｃ

--- pages/page_2.py
+++ pages/page_2.py
@@ -1,68 +1,89 @@
 import streamlit as st
-import json
-from datetime import datetime
-import pytz
-import urllib.parse
 
-# 禁止ワードのリスト
-banned_words = ["馬鹿", "禁止ワード2", "禁止ワード3"]
+class User:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 
-# ユーザーの投稿内容をチェックする関数
-def check_post_content(content):
-    # タイトルと投稿内容の禁止ワードの検出
-    for banned_word in banned_words:
-        if banned_word in content:
-            content = content.replace(banned_word, "＠" * len(banned_word))
-    return content
+class Post:
+    def __init__(self, content, author, parent=None):
+        self.content = content
+        self.author = author
+        self.parent = parent
 
-def save_post(content):
-    now = datetime.now(pytz.timezone("Asia/Tokyo"))
-    now_str = now.strftime("%Y-%m-%d %H:%M:%S")
-    post = {"content": content, "timestamp": now_str}
-    with open('posts.json', 'a') as file:
-        file.write(json.dumps(post))
-        file.write('\n')
+class Thread:
+    def __init__(self):
+        self.posts = []
 
-def load_posts():
-    with open('posts.json', 'r') as file:
-        lines = file.readlines()
-        posts = [json.loads(line.strip()) for line in lines]
-        
-        # タイムスタンプを日本時間に変換
+    def add_post(self, content, author, parent=None):
+        post = Post(content, author, parent)
+        self.posts.append(post)
+
+    def edit_post(self, post, new_content):
+        if post.author == current_user.username:
+            post.content = new_content
+        else:
+            st.error("You are not authorized to edit this post.")
+
+    def delete_post(self, post):
+        if post.author == current_user.username:
+            self.posts.remove(post)
+        else:
+            st.error("You are not authorized to delete this post.")
+
+    def display_thread(self, posts=None, indent=0):
+        if posts is None:
+            posts = self.posts
+
         for post in posts:
-            timestamp = datetime.strptime(post['timestamp'], "%Y-%m-%d %H:%M:%S")
-            timestamp = pytz.timezone("Asia/Tokyo").localize(timestamp)
-            post['timestamp'] = timestamp.strftime("%Y-%m-%d %H:%M:%S")
+            st.write(("  " * indent) + post.content)
+            if post.parent:
+                self.display_thread([p for p in self.posts if p.parent == post], indent + 1)
 
-        return posts
+# 電子掲示板の作成と使用例
+thread = Thread()
 
+# 仮想のユーザーデータベース
+user_db = [
+    User("user1", "password1"),
+    User("user2", "password2"),
+    User("user3", "password3")
+]
+
+# Streamlitアプリのレイアウトとインタラクションの作成
 def main():
-    st.title("test")
+    st.title("Threaded Bulletin Board")
+    login()
 
-    # 新規投稿の入力
-    new_post_content = st.text_area("投稿", height=100)
+def login():
+    st.subheader("Login")
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
 
-    
-    # 投稿ボタンが押された場合
-    if st.button("投稿する") and new_post_content:
-        new_post_content = check_post_content(new_post_content)
-        if "＠" in new_post_content:
-            st.warning("禁止ワードが含まれています！")
+    if st.button("Login"):
+        user = authenticate(username, password)
+        if user:
+            st.success(f"Logged in as {user.username}")
+            global current_user
+            current_user = user
+            display_posts(thread.posts)
+        else:
+            st.error("Invalid username or password.")
 
-        save_post(new_post_content)
-        st.success("投稿が保存されました！")
+def authenticate(username, password):
+    for user in user_db:
+        if user.username == username and user.password == password:
+            return user
+    return None
 
-    # 保存された投稿の表示
-    posts = load_posts()
-    st.subheader("保存された投稿")
-
-    if not posts:
-        st.info("まだ投稿がありません。")
-    else:
-        for post in posts:
-            st.subheader(post['content'])
-            st.write(post['timestamp'])  # タイムスタンプを表示
-            st.markdown("---")
+def display_posts(posts):
+    for post in posts:
+        st.write(f"Author: {post.author}")
+        st.write(post.content)
+        if post.parent:
+            st.write("Reply to:")
+            display_posts([p for p in thread.posts if p.parent == post])
+        st.write("---")
 
 if __name__ == "__main__":
     main()
