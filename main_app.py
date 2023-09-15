f58a87b7ac8a1812901a4cf7ae2bccf032cc248c
sugi-01096 <132065223+sugi-01096@users.noreply.github.com>
2023-09-15 14:26:01 +0900

    gdgdd

--- main_app.py
+++ main_app.py
@@ -8,53 +8,50 @@ import urllib.parse
 banned_words = ["馬鹿", "禁止ワード2", "禁止ワード3"]
 
 # ユーザーの投稿内容をチェックする関数
-def check_post_content(content):
+def check_post_content(title, content):
     # タイトルと投稿内容の禁止ワードの検出
     for banned_word in banned_words:
+        if banned_word in title:
+            title = title.replace(banned_word, "＠" * len(banned_word))
         if banned_word in content:
             content = content.replace(banned_word, "＠" * len(banned_word))
-    return content
+    return title, content
 
-def save_post(content, page_id):
+def save_post(title, content):
     now = datetime.now(pytz.timezone("Asia/Tokyo"))
     now_str = now.strftime("%Y-%m-%d %H:%M:%S")
-    post = {"content": content, "timestamp": now_str}
-    with open(f'posts_{page_id}.json', 'a') as file:
+    post = {"title": title, "content": content, "timestamp": now_str}
+    with open('posts.json', 'a') as file:
         file.write(json.dumps(post))
         file.write('\n')
 
-
-def load_posts(page_id):
-    try:
-        with open(f'posts_{page_id}.json', 'r') as file:
-            lines = file.readlines()
-            posts = [json.loads(line.strip()) for line in lines]
+def load_posts():
+    with open('posts.json', 'r') as file:
+        lines = file.readlines()
+        posts = [json.loads(line.strip()) for line in lines]
         
-            # タイムスタンプを日本時間に変換
-            for post in posts:
-                timestamp = datetime.strptime(post['timestamp'], "%Y-%m-%d %H:%M:%S")
-                timestamp = pytz.timezone("Asia/Tokyo").localize(timestamp)
-                post['timestamp'] = timestamp.strftime("%Y-%m-%d %H:%M:%S")
+        # タイムスタンプを日本時間に変換
+        for post in posts:
+            timestamp = datetime.strptime(post['timestamp'], "%Y-%m-%d %H:%M:%S")
+            timestamp = pytz.timezone("Asia/Tokyo").localize(timestamp)
+            post['timestamp'] = timestamp.strftime("%Y-%m-%d %H:%M:%S")
 
-            return posts
-    except FileNotFoundError:
-        return []
-
+        return posts
 
 def main():
-    st.title("test")
+    st.title("掲示板アプリ")
 
     # 新規投稿の入力
-    new_post_content = st.text_area("投稿", height=100)
-
+    new_post_content = st.text_area("管理者以外記述厳禁", height=100)
+    new_post_title = st.text_input("ページ")
     
     # 投稿ボタンが押された場合
-    if st.button("投稿する") and new_post_content:
-        new_post_content = check_post_content(new_post_content)
-        if "＠" in new_post_content:
+    if st.button("投稿する") and new_post_title and new_post_content:
+        new_post_title, new_post_content = check_post_content(new_post_title, new_post_content)
+        if "＠" in new_post_title or "＠" in new_post_content:
             st.warning("禁止ワードが含まれています！")
 
-        save_post(new_post_content)
+        save_post(new_post_title, new_post_content)
         st.success("投稿が保存されました！")
 
     # 保存された投稿の表示
@@ -65,8 +62,11 @@ def main():
         st.info("まだ投稿がありません。")
     else:
         for post in posts:
+            # 各タイトルにリンクを付けて表示
+            post_url = f"<a href='https://maichan-bord-{urllib.parse.quote(post['main'])}.streamlit.app'>{post['main']}</a>"
             st.subheader(post['content'])
             st.write(post['timestamp'])  # タイムスタンプを表示
+            st.markdown(post_url, unsafe_allow_html=True)
             st.markdown("---")
 
 if __name__ == "__main__":
