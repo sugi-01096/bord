03932fbea30ab399db491487a66f4cc4017c239d
sugi-01096 <132065223+sugi-01096@users.noreply.github.com>
2023-09-22 14:11:00 +0900

    a

---
+++ main_home.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import json
+from datetime import datetime
+import pytz
+import urllib.parse
+post_url = f"<a href='https://maichan-bord-tui.streamlit.app'>{post['main']}</a>"
+print(post_url)
