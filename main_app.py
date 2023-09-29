308ae6dfcd0bd3fe0a17c03b9a8421abf4da41be
sugi-01096 <132065223+sugi-01096@users.noreply.github.com>
2023-09-29 14:41:58 +0900

    あｓｄ

--- main_app.py
+++ main_app.py
@@ -1,11 +1,12 @@
 import streamlit as st
 
 def main():
-    st.title("外部URLへのリンク")
-       
+    st.title("外部URLへのリダイレクト")
+
     external_url = "https://maichan-bord-tui.streamlit.app/"
-    
-    st.print(f"[外部URLにアクセスする]({external_url})")
+
+    # JavaScriptを使用して外部URLにリダイレクト
+    st.write(f'<script>window.location.href = "{external_url}";</script>', unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
