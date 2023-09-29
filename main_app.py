9bd799ea7af8c67063a12651984822919506ae26
sugi-01096 <132065223+sugi-01096@users.noreply.github.com>
2023-09-29 14:39:25 +0900

    sf

--- main_app.py
+++ main_app.py
@@ -3,8 +3,9 @@ import streamlit as st
 def main():
     st.title("外部URLへのリンク")
        
-        external_url = "https://maichan-bord-tui.streamlit.app/"
-        st.print(f"[外部URLにアクセスする]({external_url})")
+    external_url = "https://maichan-bord-tui.streamlit.app/"
+    
+    st.print(f"[外部URLにアクセスする]({external_url})")
 
 if __name__ == "__main__":
     main()
