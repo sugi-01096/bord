07a58f6a0e472503f1e382afed9ff66df544aa67
sugi-01096 <132065223+sugi-01096@users.noreply.github.com>
2023-10-11 16:38:06 +0900

    あ

--- pages/gensin.py
+++ pages/gensin.py
@@ -4,8 +4,13 @@ from datetime import datetime
 import pytz
 import urllib.parse
 
-# 禁止ワードのリスト
-banned_words = "toxic.txt"
+# 禁止ワードのリストをファイルから読み込む
+def load_banned_words(filename):
+    with open(filename, 'r', encoding='utf-8') as file:
+        return [line.strip() for line in file]
+
+# toxic.txtから禁止ワードリストを読み込む
+banned_words = load_banned_words("toxic.txt")
 
 # ユーザーの投稿内容をチェックする関数
 def check_post_content(content):
@@ -15,54 +20,4 @@ def check_post_content(content):
             content = content.replace(banned_word, "＠" * len(banned_word))
     return content
 
-def save_post(content):
-    now = datetime.now(pytz.timezone("Asia/Tokyo"))
-    now_str = now.strftime("%Y-%m-%d %H:%M:%S")
-    post = {"content": content, "timestamp": now_str}
-    with open('posts.json', 'a') as file:
-        file.write(json.dumps(post))
-        file.write('\n')
-
-def load_posts():
-    with open('posts.json', 'r') as file:
-        lines = file.readlines()
-        posts = [json.loads(line.strip()) for line in lines]
-        
-        # タイムスタンプを日本時間に変換
-        for post in posts:
-            timestamp = datetime.strptime(post['timestamp'], "%Y-%m-%d %H:%M:%S")
-            timestamp = pytz.timezone("Asia/Tokyo").localize(timestamp)
-            post['timestamp'] = timestamp.strftime("%Y-%m-%d %H:%M:%S")
-
-        return posts
-
-def main():
-    st.title("原神")
-
-    # 新規投稿の入力
-    new_post_content = st.text_area("投稿", height=100)
-
-    
-    # 投稿ボタンが押された場合
-    if st.button("投稿する") and new_post_content:
-        new_post_content = check_post_content(new_post_content)
-        if "＠" in new_post_content:
-            st.warning("禁止ワードが含まれています！")
-
-        save_post(new_post_content)
-        st.success("投稿が保存されました！")
-
-    # 保存された投稿の表示
-    posts = load_posts()
-    st.subheader("保存された投稿")
-
-    if not posts:
-        st.info("まだ投稿がありません。")
-    else:
-        for post in posts:
-            st.subheader(post['content'])
-            st.write(post['timestamp'])  # タイムスタンプを表示
-            st.markdown("---")
-
-if __name__ == "__main__":
-    main()
+# 以降のコードは変更なし
