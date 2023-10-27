a50b398b00c0d200a55468ae0d01f254a173ec8e
sugi-01096 <132065223+sugi-01096@users.noreply.github.com>
2023-10-27 14:50:19 +0900

    af

--- main_app.py
+++ main_app.py
@@ -5,7 +5,7 @@ def main():
 
     # ボタンをクリックして外部URLに飛ぶ
     if st.button("外部URLにアクセス"):
-        external_url = "https://maichan-bord-tui.streamlit.app/"
+        external_url = "https://maichan-bord-ben.streamlit.app/"
         st.markdown(f"[外部URLにアクセスする]({external_url})")
 
 if __name__ == "__main__":
