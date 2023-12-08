a9bc2a07b0f96adb00e62f2d9fb700cffcd09071
ko-ki <135305163+ZUMpou@users.noreply.github.com>
2023-12-08 14:38:35 +0900

    こ

--- pages/zatudan2.py
+++ pages/zatudan2.py
@@ -1,13 +1,13 @@
 import streamlit as st
 import json
+import base64  # 追加
 from datetime import datetime
 import pytz
-import urllib.parse
 import pandas as pd
 
-# 禁止ワードをExcelファイルから読み込む。
+# 禁止ワードをExcelファイルから読み込む
 df = pd.read_excel("banned_list.xlsx", sheet_name=0)
-#禁止ワードをbanned_words に
+# 禁止ワードをbanned_words に
 banned_words = df['禁止ワード'].tolist()
 banned_words = [str(word) for word in banned_words]
 
@@ -19,19 +19,25 @@ def check_post_content(content):
             content = content.replace(banned_word, "＠" * len(banned_word))
     return content
 
-def save_post(content):
+def save_post(content, image):
     now = datetime.now(pytz.timezone("Asia/Tokyo"))
     now_str = now.strftime("%Y-%m-%d %H:%M:%S")
-    post = {"content": content, "timestamp": now_str}
-    with open('posts2.json', 'a') as file:
+
+    # 画像データをBase64にエンコード
+    image_data = None
+    if image:
+        image_data = base64.b64encode(image).decode('utf-8')
+
+    post = {"content": content, "image": image_data, "timestamp": now_str}
+    with open('posts1.json', 'a') as file:
         file.write(json.dumps(post))
         file.write('\n')
 
 def load_posts():
-    with open('posts2.json', 'r') as file:
+    with open('posts92.json', 'r') as file:
         lines = file.readlines()
         posts = [json.loads(line.strip()) for line in lines]
-        
+
         # タイムスタンプを日本時間に変換
         for post in posts:
             timestamp = datetime.strptime(post['timestamp'], "%Y-%m-%d %H:%M:%S")
@@ -41,19 +47,24 @@ def load_posts():
         return posts
 
 def main():
-    st.title("雑談2")
+    st.title("雑談２")
 
     # 新規投稿の入力
     new_post_content = st.text_area("投稿", height=100)
+    new_post_image = st.file_uploader("画像をアップロード", type=["jpg", "jpeg", "png"])
 
-    
     # 投稿ボタンが押された場合
     if st.button("投稿する") and new_post_content:
         new_post_content = check_post_content(new_post_content)
         if "＠" in new_post_content:
             st.warning("禁止ワードが含まれています！")
 
-        save_post(new_post_content)
+        # 画像がアップロードされていれば、バイナリデータに変換して保存
+        image_data = None
+        if new_post_image:
+            image_data = new_post_image.read()
+
+        save_post(new_post_content, image_data)
         st.success("投稿が保存されました！")
 
     # 保存された投稿の表示
@@ -65,6 +76,9 @@ def main():
     else:
         for post in posts:
             st.subheader(post['content'])
+            # 画像があれば表示
+            if 'image' in post and post['image'] is not None:
+                st.image(base64.b64decode(post['image']), caption="Uploaded Image", use_column_width=True)
             st.write(post['timestamp'])  # タイムスタンプを表示
             st.markdown("---")
 
